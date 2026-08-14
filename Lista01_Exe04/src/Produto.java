f59b4d06108f954ef7b99be4324aa662cd70202e
brunospbnu <brunosp423@gmail.com>
2026-08-13 23:38:52 -0300

    ajusta o programa de controle de carros e retira avisos desnesesários.

--- Lista01_Exe04/src/Produto.java
+++ Lista01_Exe04/src/Produto.java
@@ -1,7 +1,11 @@
 
 public class Produto {
+	@SuppressWarnings("unused")
 	private String nome;
+	@SuppressWarnings("unused")
 	private double precoVenda;
+	@SuppressWarnings("unused")
 	private int quantDisponivel;
+	@SuppressWarnings("unused")
 	private boolean disponibilidade;
 }
