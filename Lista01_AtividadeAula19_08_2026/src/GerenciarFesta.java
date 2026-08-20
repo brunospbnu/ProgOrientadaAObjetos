dc421d908b1911bffcacf2246d62d673f37daa39
brunospbnu <brunosp423@gmail.com>
2026-08-20 01:20:58 -0300

    avanços no programa

--- Lista01_AtividadeAula19_08_2026/src/GerenciarFesta.java
+++ Lista01_AtividadeAula19_08_2026/src/GerenciarFesta.java
@@ -1,3 +1,5 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -16,7 +18,7 @@ public class GerenciarFesta {
 		temas = new Tema[20];
 		alugueis = new Aluguel[20];
 		
-		String menuTexto = "\n Menu Principal - Escolha uma opção: \n" 
+		String menuTexto = "\n ----- Menu Principal - Escolha uma opção: ----- \n" 
 						+ "1 - inserir cliente \n"
 						+ "2 - inserir tema\n"
 						+ "3 - inserir aluguel\n"
@@ -62,7 +64,26 @@ public class GerenciarFesta {
 	}
 	
 	private void listarAlugueis() {
-		// TODO fazer o metodo para listar alugueis.
+		SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+		System.out.println("Lista de Alugueis cadastrados: \n");
+		for (int i = 0; i < alugueis.length; i++) {
+			if (alugueis[i] == null) {
+				break;
+			}
+			Tema temaAAnalisarAgora = new Tema();
+			temaAAnalisarAgora = alugueis[i].getTema();
+			String nomeTemaAMencionar = temaAAnalisarAgora.getNomeTema();
+			Cliente clienteAAnalisarAgora = new Cliente();
+			clienteAAnalisarAgora = alugueis[i].getCliente();
+			String nomeClienteAMencionar = clienteAAnalisarAgora.getNome();
+			
+			System.out.println((i+1) + ") Data: " + formato.format(alugueis[i].getData()) 
+												+ " /n Hora Inicio: " + alugueis[i].getHorainicio() 
+												+ " /n Hora Fim: " + alugueis[i].getHoraFim() 
+												+ " /n Tema: " + nomeTemaAMencionar 
+												+ " /n Cliente: " + nomeClienteAMencionar
+												+ " /n Endereço do Evento: " + alugueis[i].getEndereço());
+		}
 		
 	}
 
@@ -72,7 +93,7 @@ public class GerenciarFesta {
 			if (temas[i] == null) {
 				break;
 			}
-			System.out.println("Nome do Tema :" + temas[i].getNomeTema() + " | Valor Aluguel:  " + temas[i].getValorAluguel() + " | Cor da Toalha: " +
+			System.out.println((i+1)+ ") Nome do Tema :" + temas[i].getNomeTema() + " | Valor Aluguel:  " + temas[i].getValorAluguel() + " | Cor da Toalha: " +
 					temas[i].getCorToalha());
 			Item[] itensDoTema = temas[i].getListaDeItens();
 			for (int j = 0; j < itensDoTema.length-1; j++) {
@@ -92,7 +113,7 @@ public class GerenciarFesta {
 	private void listarClientes() {
 		System.out.println("Lista de clientes cadastrados: \n");
 		for (int i = 0; i < quantAtualClientes; i++) {
-			System.out.println("Nome:" + clientes[i].getNome() + " - " + clientes[i].getTelefone() + "\n Data Cadastro: " +
+			System.out.println((i+1)+ ") Nome:" + clientes[i].getNome() + " - " + clientes[i].getTelefone() + "\n Data Cadastro: " +
 								clientes[i].getDataPrimeiroCadastro());
 		}
 		System.out.println("\n Fim da lista \n ");
@@ -100,12 +121,89 @@ public class GerenciarFesta {
 	}
 
 	private void inserirAluguel() {
-		// TODO Fazer o metodo para inserir alugueis.
+		System.out.println(" ---- Cadastro de Aluguel ---- \n Informe a data do evento (DD/MM/AAAA): ");
+		SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+		String dataEscolhida = teclado.next();
+		boolean dataValida = false;
+		Date dataConvertida = null;
+		while (!dataValida) {
+			
+			try {
+				dataConvertida = formato.parse(dataEscolhida);
+				System.out.println("Data capturada com sucesso: " + formato.format(dataConvertida));
+				dataValida = true;
+				} 
+			
+			catch (ParseException e) {
+				System.out.println("Erro: Formato de data inválido! Use o padrão dd/mm/aaaa.");
+				dataEscolhida = teclado.next();
+				
+			}
+		}
+		
+		System.out.print("Informe a hora de inicio do evento, de 0 até 23: ");
+		int horaInicioEscolhida = teclado.nextInt();
+		while (horaInicioEscolhida < 0 || horaInicioEscolhida > 23) {
+			System.out.println("A hora de inicio deve estar entre 0 e 23 - Informe corretamente: ");
+			horaInicioEscolhida = teclado.nextInt();
+		}
+		System.out.println("Hora INICIO definida !");
+		
+		
+		System.out.println("Informe a hora de fim do evento, de 0 até 23");
+		int horaFimEscolhida = teclado.nextInt();
+		while (horaFimEscolhida < 0 || horaFimEscolhida >23) {
+			System.out.println("A hora de inicio deve estar entre 0 e 23 - Informe corretamente: ");
+			horaFimEscolhida = teclado.nextInt();
+		}
+		System.out.println("Hora FIM definida !");
+		
+		
+		System.out.println("Selecione o tema conforme a lista abaixo: ");
+		listarTemas();
+		System.out.print("Informe o número do Tema escolhido: ");
+		int temaEscolhido = teclado.nextInt();
+		temaEscolhido = temaEscolhido - 1;
+		Tema temaAtual = new Tema();
+		temaAtual = temas[temaEscolhido];
+		System.out.println("Tema definido !");
+		
+		
+		System.out.println("Selecione o cliente conforme a lista abaixo: ");
+		listarClientes();
+		System.out.print("Informe o número do Cliente escolhido: ");
+		int clienteEscolhido = teclado.nextInt();
+		clienteEscolhido = clienteEscolhido - 1;
+		Cliente clienteAtual = new Cliente();
+		clienteAtual = clientes[clienteEscolhido];
+		System.out.println("Cliente definido !");
+		
+		
+		System.out.print("Informe o endereço do evento em texto livre: ");
+		String endereçoDigitado = teclado.next();
+		while (endereçoDigitado == null || endereçoDigitado.isEmpty() || endereçoDigitado.length() < 3) {
+			System.out.print("Endereço inválido, informe um com 3 letras ou mais: ");
+			endereçoDigitado = teclado.next();
+		}
+		
+		
+		Aluguel aluguelAtual = new Aluguel();
+		aluguelAtual.setData(dataConvertida);
+		aluguelAtual.setHorainicio(horaInicioEscolhida);
+		aluguelAtual.setHoraFim(horaFimEscolhida);
+		aluguelAtual.setTema(temaAtual);
+		aluguelAtual.setCliente(clienteAtual);
+		aluguelAtual.setEndereço(endereçoDigitado);
+		
+		alugueis[quantAtualAluguel] = aluguelAtual;
+		quantAtualAluguel++;
+		
+		System.out.println("ALUGUEL CADASTRADO !!! ");
 		
 	}
 
 	private void inserirTema() {
-		System.out.print("Cadastro de Tema para os alugueis\n Digite o nome do Tema: ");
+		System.out.print("----- Cadastro de Tema para os alugueis ----- \n Digite o nome do Tema: ");
 		String nomeTema = teclado.next();
 		while (nomeTema.length() <3) {
 			System.out.print("O nome do Tema deve ter 3 letras ou mais - Informe um corretamente: ");
